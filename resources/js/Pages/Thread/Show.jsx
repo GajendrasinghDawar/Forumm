773e245b1c7c45937ae10483cd7f50903a4597cd
GajendrasinghDawar <gajendrasinghdawar@gmail.com>
2024-07-15 18:15:29 +0530

    improvement grid layout show.jsx

--- resources/js/Pages/Thread/Show.jsx
+++ resources/js/Pages/Thread/Show.jsx
@@ -7,45 +7,44 @@ export default function Show({ thread }) {
     let { props } = usePage();
 
     return (
-        <>
-            <Head title={thread.data.title} />
+        <> <Head title={ thread.data.title } />
             <Container>
                 <div className="grid w-full grid-cols-7  grid-flow-col-dense font-inter">
-                    <section className="py-2 md:col-span-3 md:col-end-5 sm:col-start-1 sm:col-end-8">
-                        <h1 className=" rounded-md">{thread.data.title}</h1>
-                        <p className="mt-4">{thread.data.body}</p>
+                    <section className="py-2 md:col-span-3 md:col-end-5 col-start-1 col-end-8">
+                        <h1 className=" rounded-md">{ thread.data.title }</h1>
+                        <p className="mt-4">{ thread.data.body }</p>
                         <article>
                             <h2 className="my-4 font-semibold ">Replies</h2>
-                            {props.auth.user && (
-                                <ReplyForm threadId={thread.data.id} />
-                            )}
-                            {thread.data.replies?.map((reply) => (
+                            { props.auth.user && (
+                                <ReplyForm threadId={ thread.data.id } />
+                            ) }
+                            { thread.data.replies?.map((reply) => (
                                 <div
-                                    key={reply.id}
+                                    key={ reply.id }
                                     className="bg-gray-100 my-4 p-2 border border-sand-sand4 rounded-md space-y-2"
                                 >
                                     <section className="flex justify-between items-center  py-1 ">
                                         <p>
                                             <span> replied by</span>
                                             <a href="#" className="mx-1">
-                                                {reply.user?.name}
+                                                { reply.user?.name }
                                             </a>
-                                            <span> {reply.created_at}</span>
+                                            <span> { reply.created_at }</span>
                                         </p>
                                         <div>
                                             <span>
-                                                Favorites{" "}
-                                                {reply.favorites_count}
+                                                Favorites's { " " }
+                                                { reply.favorites_count }
                                             </span>
-                                            {props.auth?.user && (
+                                            { props.auth?.user && (
                                                 <Link
                                                     as="button"
-                                                    href={route(
+                                                    href={ route(
                                                         "replies.favorite",
                                                         {
                                                             reply: reply.id,
                                                         }
-                                                    )}
+                                                    ) }
                                                     method="post"
                                                     preserveScroll
                                                     className="ml-1 inline-block bg-sand-sand5 hover:bg-sand-sand6 transition-colors  py-1 w-min h-min px-2  text-xs rounded"
@@ -57,25 +56,25 @@ export default function Show({ thread }) {
                                                 >
                                                     Favorite
                                                 </Link>
-                                            )}
+                                            ) }
                                         </div>
                                     </section>
-                                    <p>{reply.body}</p>
+                                    <p>{ reply.body }</p>
                                 </div>
-                            ))}
+                            )) }
                         </article>
                     </section>
-                    <section className="sm:hidden md:block bg-gray-100 border border-sand-sand4  mt-2  h-[250px] z-0	w-70 rounded-md  sticky top-20 col-start-6  col-end-8">
+                    <section className="hidden md:block bg-gray-100 border border-sand-sand4  mt-2  h-[250px] z-0	w-70 rounded-md  sticky top-20 col-start-6  col-end-8">
                         <div className="w-full min-h-full  p-2 space-y-2">
                             <p>
-                                This thread was published{" "}
-                                {thread.data.created_at} by
+                                This thread was published{ " " }
+                                { thread.data.created_at } by
                                 <a href="#" className="mx-1">
-                                    {thread.data.author}
+                                    { thread.data.author }
                                 </a>
                             </p>
                             <p>
-                                It has currently has {thread.data.replies_count}{" "}
+                                It has currently has { thread.data.replies_count }{ " " }
                                 comments.
                             </p>
                         </div>
@@ -85,5 +84,3 @@ export default function Show({ thread }) {
         </>
     );
 }
-
-
