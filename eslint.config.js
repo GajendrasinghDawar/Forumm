3cd9252fabdbacd59f534756fc8c2d54e6309e83
GajendrasinghDawar <gajendrasinghdawar@gmail.com>
2024-07-25 19:26:16 +0530

    adjust ssr.jsx for ssr

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       globals: {
         ...globals.browser,
         "globals": true,
+        "global": true,
         "route": true,
         "window": true
       }
--- resources/js/ssr.jsx
+++ resources/js/ssr.jsx
@@ -4,14 +4,36 @@ import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { route } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+
+const appName = import.meta.env.VITE_APP_NAME || "Forumm";
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+        resolve: async (name) => {
+            let page = resolvePageComponent(
+                `./Pages/${name}.jsx`,
+                import.meta.glob("./Pages/**/*.jsx")
+            );
+
+            let newpage = await page;
+
+            newpage.default.layout =
+                newpage.default.layout ||
+                ((page) => (
+                    <Authenticated
+                        children={ page }
+                        user={ page.props.auth.user }
+                    // permissions={page.props.permissions}
+                    />
+                ));
+
+            return newpage;
+        },
+
         setup: ({ App, props }) => {
             global.route = (name, params, absolute) =>
                 route(name, params, absolute, {
